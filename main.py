c5a33458b50ed96300e664540fcef030640d3dcd
AleksandrsKozaletovs <125273328+AleksandrsKozaletovs@users.noreply.github.com>
2023-02-16 13:46:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def main():
         mismatch = find_mismatch(i)
     elif text == "F":
         print("In progress") 
-    if text.isnumeric() == true:
+    if text.isnumeric() == True:
         return
     else:    
         mismatch = find_mismatch(text)
